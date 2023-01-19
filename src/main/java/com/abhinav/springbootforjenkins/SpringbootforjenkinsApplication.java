10a3105caf3e95d8cf7931cd4102d423fb32f62e
Abhinav <abhinav.anant14@gmail.com>
2023-01-19 15:49:05 +0530

    added one more line on 19-01-2023 at 3:48PM IST

--- src/main/java/com/abhinav/springbootforjenkins/SpringbootforjenkinsApplication.java
+++ src/main/java/com/abhinav/springbootforjenkins/SpringbootforjenkinsApplication.java
@@ -28,6 +28,7 @@ public class SpringbootforjenkinsApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		logger.info("Abhinav | Its for Jenkins testing again=====>>>>>");
 		logger.info("Abhinav | hooked jenkins with github ----->>>>>");
+		logger.info("Abhinav | hooked jenkins with github on 19-01-2023 at 3:48PM IST ----->>>>>");
 	}
 
 }
